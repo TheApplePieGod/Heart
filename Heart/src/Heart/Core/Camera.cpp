a9bbd136bac68df40aafab66581b604c2a57f426
TheApplePieGod <fishplaysgame@gmail.com>
2021-08-21 16:31:18 -0500

    Camera base class

---
+++ Heart/src/Heart/Core/Camera.cpp
@@ -0,0 +1,44 @@
+#include "htpch.h"
+#include "Camera.h"
+
+#include "glm/gtc/matrix_transform.hpp"
+
+namespace Heart
+{
+    Camera::~Camera()
+    {
+
+    }
+
+    void Camera::UpdateProjectionMatrix()
+    {
+        switch (m_ProjectionType)
+        {
+            default: break;
+            case ProjectionType::Perspective:
+            {
+                m_ProjectionMatrix = glm::perspective(glm::radians(m_FOV), m_AspectRatio, m_NearClip, m_FarClip);
+            } break;
+
+            case ProjectionType::Orthographic:
+            {
+                m_ProjectionMatrix = glm::transpose(glm::ortho(-0.5f * m_OrthoWidth, 0.5f * m_OrthoWidth, 0.5f * m_OrthoHeight, -0.5f * m_OrthoHeight, m_NearClip, m_FarClip));
+            } break;
+
+            case ProjectionType::OrthographicFlat:
+            {
+                f32 l = -0.5f * m_OrthoWidth;
+                f32 r = 0.5f * m_OrthoWidth;
+                f32 b = 0.5f * m_OrthoHeight;
+                f32 t = -0.5f * m_OrthoHeight;
+                m_ProjectionMatrix = glm::mat4(
+                    2.f / (r - l), 0, 0, 0,
+                    0, 2.f / (t - b), 0, 0,
+                    0, 0, 1.f / (m_NearClip - m_FarClip), 0,
+                    (l + r) / (l - r), (t + b) / (b - t), m_NearClip / (m_NearClip - m_FarClip), 1
+                );
+                m_ProjectionMatrix = glm::transpose(m_ProjectionMatrix);
+            } break;
+        }
+    }
+}
---
+++ Heart/src/Heart/Core/Camera.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "glm/mat4x4.hpp"
+#include "glm/vec3.hpp"
+
+/*
+* Default coordinate system
+*---------------------------
+*                   
+*        +Y    +Z
+*         |  /
+*         |/
+*         ------- +X
+*
+*/
+
+namespace Heart
+{
+    class Camera
+    {
+    public:
+        enum class ProjectionType
+        {
+            Perspective = 0, Orthographic = 1, OrthographicFlat = 2
+        };
+
+    public:
+        Camera(f32 fov, f32 nearClip, f32 farClip, f32 aspectRatio)
+            : m_ProjectionType(ProjectionType::Perspective), m_FOV(fov), m_NearClip(nearClip), m_FarClip(farClip), m_AspectRatio(aspectRatio)
+        { UpdateProjectionMatrix(); }
+
+        Camera(ProjectionType type, f32 width, f32 height, f32 nearClip, f32 farClip, f32 aspectRatio)
+            : m_ProjectionType(type), m_OrthoWidth(width), m_OrthoHeight(height), m_NearClip(nearClip), m_FarClip(farClip), m_AspectRatio(aspectRatio)
+        { UpdateProjectionMatrix(); }
+
+        ~Camera();
+
+        inline ProjectionType GetType() const { return m_ProjectionType; }
+        inline void UpdateWidth(f32 width) { m_OrthoWidth = width; UpdateProjectionMatrix(); }
+        inline void UpdateHeight(f32 height) { m_OrthoHeight = height; UpdateProjectionMatrix(); }
+        inline void UpdateAspectRatio(f32 ratio) { m_AspectRatio = ratio; UpdateProjectionMatrix(); }
+        inline void UpdateFOV(f32 fov) { m_FOV = fov; UpdateProjectionMatrix(); }
+        inline void UpdateNearClip(f32 clip) { m_NearClip = clip; UpdateProjectionMatrix(); }
+        inline void UpdateFarClip(f32 clip) { m_FarClip = clip; UpdateProjectionMatrix(); }
+        inline void UpdateProjectionType(ProjectionType type) { m_ProjectionType = type; UpdateProjectionMatrix(); }
+
+    protected:
+        void UpdateProjectionMatrix();
+
+    protected:
+        const glm::vec3 m_XAxis = { 1.f, 0.f, 0.f };
+        const glm::vec3 m_YAxis = { 0.f, 1.f, 0.f };
+        const glm::vec3 m_ZAxis = { 0.f, 0.f, 1.f };
+        const glm::vec3 m_UpVector = m_YAxis;
+        const glm::vec3 m_DefaultForwardVector = m_ZAxis;
+
+        f32 m_FOV, m_OrthoWidth, m_OrthoHeight, m_NearClip, m_FarClip, m_AspectRatio;
+        ProjectionType m_ProjectionType;
+        glm::mat4 m_ProjectionMatrix;
+    };
+}
