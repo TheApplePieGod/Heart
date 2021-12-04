c57d3f5befaf219137be6f5527775a2219629c0e
TheApplePieGod <fishplaysgame@gmail.com>
2021-12-04 12:54:27 -0600

    Documentation progress

--- Heart/src/Heart/Asset/AssetManager.h
+++ Heart/src/Heart/Asset/AssetManager.h
@@ -5,15 +5,35 @@
 
 namespace Heart
 {
+    /**
+     * @brief The static manager of all assets in the engine.
+     * 
+     * The manager will automatically handle the lifecycle of assets and their data
+     * while providing a simple interface for retrieving them. The manager stores two different
+     * types of assets: resources and regular assets. A resource is still an asset, but it is
+     * one that explicitly builds with the engine and therefore is assumed to always exist.
+     * A resource always lives in the {s_ResourceDirectory} directory in the executable path.
+     * A regular asset, however, is not assumed to exist and lives rooted under the {s_AssetsDirectory}
+     * directory, which can be modified at runtime.
+     */
     class AssetManager
     {
     public:
+        /**
+         * @brief 
+         * 
+         */
         struct AssetEntry
         {
             Ref<Asset> Asset;
             u64 LoadedFrame;
             bool Persistent;
         };
+
+        /**
+         * @brief 
+         * 
+         */
         struct UUIDEntry
         {
             std::string Path;
@@ -22,10 +42,19 @@ namespace Heart
         };
 
     public:
+        /*! @brief Initialize the manager */
         static void Initialize();
+
+        /*! @brief Shutdown the manager */
         static void Shutdown();
+
+        /*! @brief Call Unload() on all of the registered assets */
         static void UnloadAllAssets();
+
+        /*! @brief Call Load() on all of the registered assets */
         static void LoadAllAssets();
+
+        /*! @brief Initialize the manager */
         static void UnloadAllResources();
         static void LoadAllResources();
         static void OnUpdate();
@@ -34,6 +63,22 @@ namespace Heart
         inline static const std::unordered_map<UUID, UUIDEntry>& GetUUIDRegistry() { return s_UUIDs; }
         inline static std::string GetAbsolutePath(const std::string& relative) { return std::filesystem::path(s_AssetsDirectory).append(relative).generic_u8string(); }
 
+        /**
+         * @brief Register an asset in the manager's registry.
+         * 
+         * Because these resources and regular assets are stored independently, it is impossible
+         * to have naming collisions. For example, registering a resource at 'folder/Test.png' will
+         * not prevent you from registering a regular asset with the same path because they are distinguished
+         * by the isResource flag.
+         * 
+         * @note Registering the asset will not load its data.
+         * 
+         * @param type The type of the asset
+         * @param path The path of the asset relative to the project directory.
+         * @param persistent Mark this asset as persistent (will never automatically unload).
+         * @param isResource Store this asset as a resource.
+         * @return The UUID of the newly registered asset.
+         */
         static UUID RegisterAsset(Asset::Type type, const std::string& path, bool persistent = false, bool isResource = false);
         static void RegisterAssetsInDirectory(const std::filesystem::path& directory, bool persistent = false, bool isResource = false);
 
