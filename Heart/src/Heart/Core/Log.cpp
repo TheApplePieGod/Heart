3924c17a07962e1bfa5842d34c24c8cbb1e7bffb
TheApplePieGod <fishplaysgame@gmail.com>
2021-12-18 20:38:35 -0600

    File logging

--- Heart/src/Heart/Core/Log.cpp
+++ Heart/src/Heart/Core/Log.cpp
@@ -1,6 +1,9 @@
 #include "hepch.h"
 #include "Log.h"
 
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
 namespace Heart
 {
     Ref<spdlog::logger> Logger::s_EngineLogger;
@@ -8,15 +11,19 @@ namespace Heart
 
     void Logger::Initialize()
     {
-        auto console_sink = CreateRef<spdlog::sinks::stdout_color_sink_mt>();
-        console_sink->set_pattern("%^[%T] %n: %v%$");
+        std::vector<spdlog::sink_ptr> logSinks = {
+            CreateRef<spdlog::sinks::stdout_color_sink_mt>(),
+            CreateRef<spdlog::sinks::basic_file_sink_mt>("Heart.log", true)
+        };
+        logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+        logSinks[1]->set_pattern("[%T] [%l] %n: %v");
 
-        s_EngineLogger = CreateRef<spdlog::logger>("ENGINE", console_sink);
+        s_EngineLogger = CreateRef<spdlog::logger>("ENGINE", logSinks.begin(), logSinks.end());
         spdlog::register_logger(s_EngineLogger);
-        s_EngineLogger->set_level(spdlog::level::info);
-        s_EngineLogger->flush_on(spdlog::level::info);
+        s_EngineLogger->set_level(spdlog::level::trace);
+        s_EngineLogger->flush_on(spdlog::level::trace);
 
-        s_ClientLogger = CreateRef<spdlog::logger>("CLIENT", console_sink);
+        s_ClientLogger = CreateRef<spdlog::logger>("CLIENT", logSinks.begin(), logSinks.end());
         spdlog::register_logger(s_ClientLogger);
         s_ClientLogger->set_level(spdlog::level::trace);
         s_ClientLogger->flush_on(spdlog::level::trace);
