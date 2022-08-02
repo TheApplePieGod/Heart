f6d0b4ce4e3693e09864c56fa5efb0545927c335
TheApplePieGod <fishplaysgame@gmail.com>
2022-08-02 13:53:41 -0500

    Asset registry filtering

--- HeartEditor/src/HeartEditor/Widgets/AssetRegistry.cpp
+++ HeartEditor/src/HeartEditor/Widgets/AssetRegistry.cpp
@@ -4,7 +4,6 @@
 #include "Heart/Container/HString.h"
 #include "Heart/Asset/AssetManager.h"
 #include "Heart/Util/ImGuiUtils.h"
-#include "imgui/imgui.h"
 
 namespace HeartEditor
 {
@@ -20,20 +19,38 @@ namespace Widgets
         ImGui::Begin(m_Name.c_str(), &m_Open);
 
         ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(2.f, 2.f));
-        if (ImGui::BeginTable("RegistryTable", 4, ImGuiTableFlags_RowBg | ImGuiTableFlags_Resizable))
+        if (ImGui::BeginTable("RegistryTable", 4, ImGuiTableFlags_RowBg | ImGuiTableFlags_Resizable | ImGuiTableFlags_SizingFixedFit))
         {
-            ImGui::TableSetupColumn("UUID", ImGuiTableColumnFlags_WidthFixed);
-            ImGui::TableSetupColumn("Type", ImGuiTableColumnFlags_WidthFixed);
-            ImGui::TableSetupColumn("Is Resource", ImGuiTableColumnFlags_WidthFixed);
-            ImGui::TableSetupColumn("Path", ImGuiTableColumnFlags_WidthFixed);
-            ImGui::TableHeadersRow();
+            ImGui::TableNextColumn();
+            ImGui::Text("UUID");
+            DrawTextFilter(m_UUIDFilter, "##idfilter");
+
+            ImGui::TableNextColumn();
+            ImGui::Text("Type");
+            DrawAssetTypeFilter();
+
+            ImGui::TableNextColumn();
+            ImGui::Text("Is Resource");
+            DrawIsResourceFilter();
+
+            ImGui::TableNextColumn();
+            ImGui::Text("Path");
+            DrawTextFilter(m_PathFilter, "##pathfilter");
 
             auto& registry = Heart::AssetManager::GetUUIDRegistry();
             for (auto& pair : registry)
             {
+                Heart::HString uuid = std::to_string(pair.first);
+
+                if (!PassAssetTypeFilter((u32)pair.second.Type) ||
+                    !PassIsResourceFilter(pair.second.IsResource) ||
+                    !m_UUIDFilter.PassFilter(uuid.DataUTF8()) ||
+                    !m_PathFilter.PassFilter(pair.second.Path.c_str())
+                )
+                    continue;
+
                 ImGui::TableNextRow();
 
-                Heart::HString uuid = std::to_string(pair.first);
                 Heart::HString id1 = "##" + uuid;
                 Heart::HString id2 = id1 + "p";
 
@@ -42,21 +59,7 @@ namespace Widgets
                 ImGui::InputText(id1.DataUTF8(), (char*)uuid.DataUTF8(), uuid.GetCountUTF8(), ImGuiInputTextFlags_ReadOnly);
 
                 ImGui::TableNextColumn();
-                switch (pair.second.Type)
-                {
-                    default:
-                    { ImGui::Text("None"); } break;
-                    case Heart::Asset::Type::Texture:
-                    { ImGui::Text("Texture"); } break;
-                    case Heart::Asset::Type::Shader:
-                    { ImGui::Text("Shader"); } break;
-                    case Heart::Asset::Type::Mesh:
-                    { ImGui::Text("Mesh"); } break;
-                    case Heart::Asset::Type::Material:
-                    { ImGui::Text("Material"); } break;
-                    case Heart::Asset::Type::Scene:
-                    { ImGui::Text("Scene"); } break;
-                }
+                ImGui::Text(HE_ENUM_TO_STRING(Heart::Asset, pair.second.Type));
 
                 ImGui::TableNextColumn();
                 ImGui::Text(pair.second.IsResource ? "true" : "false");
@@ -75,6 +78,104 @@ namespace Widgets
 
         ImGui::End();
         ImGui::PopStyleVar();
+    }
+
+    void AssetRegistry::DrawFilterPopup(const char* popupName, bool focusOnOpen, std::function<void()>&& drawCallback, std::function<void()>&& clearCallback)
+    {
+        ImGui::SameLine();
+
+        Heart::HString b1id = Heart::HString("V##v") + popupName;
+        Heart::HString b2id = Heart::HString("X##x") + popupName;
+
+        bool popupOpened = ImGui::Button(b1id.DataUTF8());
+        if (popupOpened)
+            ImGui::OpenPopup(popupName);
+
+        ImGui::SameLine(0.f, 3.f);
+        if (ImGui::Button(b2id.DataUTF8()))
+            clearCallback();
+
+        if (ImGui::BeginPopup(popupName, ImGuiWindowFlags_HorizontalScrollbar))
+        {
+            drawCallback();
+            if (focusOnOpen && popupOpened)
+                ImGui::SetKeyboardFocusHere(-1);
+            ImGui::EndPopup();
+        }
+    }
+
+    void AssetRegistry::DrawTextFilter(ImGuiTextFilter& filter, const char* popupName)
+    {
+        DrawFilterPopup(
+            popupName,
+            true,
+            [&filter] ()
+            {
+                if (filter.Draw())
+                    ImGui::SetKeyboardFocusHere(-1);
+            },
+            [&filter] ()
+            {
+                filter.Clear();
+            }
+        );
+    }
+
+    void AssetRegistry::DrawAssetTypeFilter()
+    {
+        auto& filter = m_AssetTypeFilter;
+        DrawFilterPopup(
+            "##typefilter",
+            false,
+            [&filter] ()
+            {
+                u32 index = 0;
+                for (auto str : Heart::Asset::TypeStrings)
+                {
+                    if (ImGui::Selectable(str, filter == index))
+                        filter = index;
+                    index++;
+                }
+            },
+            [&filter] ()
+            {
+                filter = 0;
+            }
+        );
+    }
+
+    void AssetRegistry::DrawIsResourceFilter()
+    {
+        auto& filter = m_IsResourceFilter;
+        DrawFilterPopup(
+            "##resfilter",
+            false,
+            [&filter] ()
+            {
+                if (ImGui::Selectable("N/A", filter == 0))
+                    filter = 0;
+                if (ImGui::Selectable("true", filter == 2))
+                    filter = 2;
+                if (ImGui::Selectable("false", filter == 1))
+                    filter = 1;
+            },
+            [&filter] ()
+            {
+                filter = 0;
+            }
+        );
+    }
+
+    bool AssetRegistry::PassAssetTypeFilter(u32 type)
+    {
+        return m_AssetTypeFilter == 0 ||
+               m_AssetTypeFilter == type;
+    }
+
+    bool AssetRegistry::PassIsResourceFilter(bool isResource)
+    {
+        return m_IsResourceFilter == 0 ||
+               (bool)(m_IsResourceFilter - 1) == isResource;
     }
 }
 }
--- HeartEditor/src/HeartEditor/Widgets/AssetRegistry.h
+++ HeartEditor/src/HeartEditor/Widgets/AssetRegistry.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "HeartEditor/Widgets/Widget.h"
+#include "imgui/imgui.h"
 
 namespace HeartEditor
 {
@@ -16,7 +17,19 @@ namespace Widgets
         void OnImGuiRender() override;
 
     private:
+        void DrawFilterPopup(const char* popupName, bool focusOnOpen, std::function<void()>&& drawCallback, std::function<void()>&& clearCallback);
+        void DrawTextFilter(ImGuiTextFilter& filter, const char* popupName);
+        void DrawAssetTypeFilter();
+        void DrawIsResourceFilter();
 
+        bool PassAssetTypeFilter(u32 type);
+        bool PassIsResourceFilter(bool isResource);
+
+    private:
+        ImGuiTextFilter m_UUIDFilter;
+        ImGuiTextFilter m_PathFilter;
+        u32 m_AssetTypeFilter = 0;
+        int m_IsResourceFilter = 0;
     };
 }
 }
